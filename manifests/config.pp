9a68d2712d6ec4a7ffb9e1f8a80ca5fdc9371671
Aran Cox <arancox@gmail.com>
2021-01-16 16:34:33 -0600

    add public cert handling

--- manifests/config.pp
+++ manifests/config.pp
@@ -21,15 +21,17 @@ class openssh::config
       value => $openssh::banner_path,
     }
   }
+  
   $::openssh::supported_key_types.each |String $keytype| {
     $private_key = lookup("openssh::${keytype}_private_key",Variant[String,Undef],'first',undef)
     $public_key = lookup("openssh::${keytype}_public_key",Variant[String,Undef],'first',undef)
-    if( $private_key !~ Undef and $public_key !~ Undef ) {
-      openssh::keypair { "${::fqdn} ${keytype} key":
-        keytype     => $keytype,
-        private_key => $private_key,
-        public_key  => $public_key,
-      }
+    $public_cert = lookup("openssh::${keytype}_public_cert",Variant[String,Undef],'first',undef)
+
+    openssh::keypair { "${::fqdn} ${keytype} key":
+      keytype     => $keytype,
+      private_key => $private_key,
+      public_key  => $public_key,
+      public_cert => $public_cert,
     }
   }
 }
--- manifests/keypair.pp
+++ manifests/keypair.pp
@@ -3,8 +3,9 @@
 define openssh::keypair
 (
   Enum['rsa1','rsa','dsa','ecdsa','ed25519'] $keytype,
-  String $private_key,
-  String $public_key,
+  Variant[String,Undef] $private_key,
+  Variant[String,Undef] $public_key,
+  Variant[String,Undef] $public_cert = undef,
   Stdlib::Filemode $mode = $::openssh::private_key_mode,
   Variant[String,Integer] $group = $::openssh::private_key_group,
   Variant[String,Integer] $owner = $::openssh::private_key_owner,
@@ -25,25 +26,37 @@ define openssh::keypair
     'ed25519' => 'ssh-ed25519',
   }
   $key="ssh_host_${string}key"
-  file { "${ssh_etc}/${key}":
-    owner     => $owner,
-    mode      => $mode,
-    group     => $group,
-    content   => $private_key,
-    show_diff => false,
+  if($private_key) {
+    file { "${ssh_etc}/${key}":
+      owner     => $owner,
+      mode      => $mode,
+      group     => $group,
+      content   => $private_key,
+      show_diff => false,
+    }
   }
-  # if the key is the full string, use it as is
-  if($public_key =~ "^(ssh|ecdsa)-") {
-    $public_key_string = "${public_key}\n"
+  if($public_key) {
+    # if the key is the full string, use it as is
+    if($public_key =~ "^(ssh|ecdsa)-") {
+      $public_key_string = "${public_key}\n"
+    }
+    else {
+      # if not prepend the ident string as above and the hostname at the end
+      $public_key_string = "${ident} ${public_key} ${::fqdn}\n"
+    }
+    file { "${ssh_etc}/${key}.pub":
+      owner   => $owner,
+      mode    => '0644',
+      group   => $group,
+      content => $public_key_string,
+    }
   }
-  else {
-    # if not prepend the ident string as above and the hostname at the end
-    $public_key_string = "${ident} ${public_key} ${::fqdn}\n"
-  }
-  file { "${ssh_etc}/${key}.pub":
-    owner   => $owner,
-    mode    => '0644',
-    group   => $group,
-    content => $public_key_string,
-  }
+  if($public_cert) {
+    file { "${ssh_etc}/${key}-cert.pub":
+      owner   => $owner,
+      mode    => '0644',
+      group   => $group,
+      content => "${public_cert}\n",
+    }
+  }    
 }
