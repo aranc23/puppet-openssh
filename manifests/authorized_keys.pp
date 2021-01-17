f4e75e05e95cfdf52b61e95445704083a82988f5
Aran Cox <arancox@gmail.com>
2021-01-16 21:46:37 -0600

    tidy up docs

--- manifests/authorized_keys.pp
+++ manifests/authorized_keys.pp
@@ -1,9 +1,5 @@
-# @summary A short summary of the purpose of this class
+# @summary calls create_rsources on the ssh_authorized_keys hash
 #
-# A description of what this class does
-#
-# @example
-#   include openssh::authorized_keys
 class openssh::authorized_keys {
   create_resources(
     'ssh_authorized_key',
