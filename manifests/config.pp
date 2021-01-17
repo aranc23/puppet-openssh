ce13f14798c3d4943cac8078c6d696c7169284d1
Aran Cox <arancox@gmail.com>
2021-01-16 21:46:18 -0600

    simplify invocations of keypair

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,15 +23,11 @@ class openssh::config
   }
   
   $::openssh::supported_key_types.each |String $keytype| {
-    $private_key = lookup("openssh::${keytype}_private_key",Variant[String,Undef],'first',undef)
-    $public_key = lookup("openssh::${keytype}_public_key",Variant[String,Undef],'first',undef)
-    $public_cert = lookup("openssh::${keytype}_public_cert",Variant[String,Undef],'first',undef)
-
     openssh::keypair { "${::fqdn} ${keytype} key":
       keytype     => $keytype,
-      private_key => $private_key,
-      public_key  => $public_key,
-      public_cert => $public_cert,
+      private_key => lookup("openssh::${keytype}_private_key",Variant[String,Undef],'first',undef),
+      public_key  => lookup("openssh::${keytype}_public_key",Variant[String,Undef],'first',undef),
+      public_cert => lookup("openssh::${keytype}_public_cert",Variant[String,Undef],'first',undef),
     }
   }
 }
