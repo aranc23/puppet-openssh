8014e62cf4e0218778def4e2b8c52d6e15935fa7
Aran Cox <arancox@gmail.com>
2021-01-18 15:14:18 -0600

    add fixtures for testing, several more spec tests

--- spec/classes/openssh_spec.rb
+++ spec/classes/openssh_spec.rb
@@ -12,10 +12,40 @@ describe 'openssh' do
     it { is_expected.to contain_class('openssh::ssh_config') }
     it { is_expected.to contain_class('openssh::known_hosts') }
   end
+  context "configure sshd and ssh client" do
+    let(:params) {
+      {
+        :sshd_config => {
+          'Protocol' => {
+            'value' => '2',
+          },
+        },
+        :ssh_config => {
+          'HashKnownHosts' => {
+            'value' => 'no',
+        },
+        },
+      }
+    }
+    it { is_expected.to contain_sshd_config('Protocol') }
+    it { is_expected.to contain_ssh_config('HashKnownHosts') }
+  end
+  context "create banner" do
+    let(:params) {
+      {
+        :banner_path => '/etc/ssh/banner',
+        :banner => 'logins are logged',
+      }
+    }
+    it { is_expected.to contain_file('/etc/ssh/banner').with_content('logins are logged') }
+  end
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
-
+      let(:facts) do
+        os_facts.merge({
+                         :fqdn => 'test.example.com',
+                       })
+      end
       it { is_expected.to compile }
       it { is_expected.to contain_service('sshd') }
       case os_facts[:osfamily]
@@ -27,6 +57,30 @@ describe 'openssh' do
         %w(openssh openssh-server openssh-clients).each do
           |p| it { is_expected.to contain_package(p) }
         end
+      end
+      context "manage host keys" do
+        let(:node) { 'test.example.com' }
+        let(:params) {
+          {
+            :rsa_private_key => 'some key',
+            :rsa_public_cert => 'some key',
+            :rsa_public_key => 'some key',
+            :dsa_private_key => 'some key',
+            :dsa_public_key => 'some key',
+            :dsa_public_cert => 'some key',
+            :ecdsa_private_key => 'some key',
+            :ecdsa_public_key => 'some key',
+            :ecdsa_public_cert => 'some key',
+            :ed25519_private_key => 'some key',
+            :ed25519_public_key => 'some key',
+            :ed25519_public_cert => 'some key',
+            :supported_key_types => ['rsa','dsa','ecdsa','ed25519'],
+          }
+        }
+        it { is_expected.to contain_openssh__keypair("test.example.com rsa key").with(:keytype => 'rsa') }
+        it { is_expected.to contain_openssh__keypair("test.example.com dsa key").with(:keytype => 'dsa') }
+        it { is_expected.to contain_openssh__keypair("test.example.com ecdsa key").with(:keytype => 'ecdsa') }
+        it { is_expected.to contain_openssh__keypair("test.example.com ed25519 key").with(:keytype => 'ed25519') }
       end
     end
   end
--- spec/defines/keypair_spec.rb
+++ spec/defines/keypair_spec.rb
@@ -4,12 +4,78 @@ require 'spec_helper'
 
 describe 'openssh::keypair' do
   let(:title) { 'namevar' }
-  let(:params) do
-    { 'keytype' => 'rsa', 'private_key' => nil, 'public_key' => nil }
-  end
 
+  {
+    'rsa'     => 'ssh-rsa public foo.example.com',
+    'dsa'     => 'ssh-dss public foo.example.com',
+    'ecdsa'   => 'ecdsa-sha2-nistp512 public foo.example.com',
+    'ed25519' => 'ssh-ed25519 public foo.example.com',
+  }.each do |keytype,public_key|
+    context "keytype #{keytype} with full public key" do
+      let(:node) { 'foo.example.com' }
+      let(:params) {
+        {
+          'keytype' => keytype,
+          'private_key' => nil,
+          'public_key' => public_key,
+          'mode' => '0666',
+          'owner' => 'bob',
+          'group' => 0,
+          'ssh_etc' => '/etc/ssh',
+        } }
+      case keytype
+      when 'rsa'
+        keyfile = 'ssh_host_rsa_key'
+      when 'dsa'
+        keyfile = "ssh_host_dsa_key"
+      when 'ecdsa'
+        keyfile = "ssh_host_ecdsa_key"
+      when 'ed25519'
+        keyfile = "ssh_host_ed25519_key"
+      end
+      
+      it { is_expected.to contain_file("/etc/ssh/#{keyfile}.pub").with('mode' => '0644','owner' => 'bob', 'group' => 0,'content' => "#{public_key}\n") }
+    end
+  end
+  ['rsa','dsa','ecdsa','ed25519'].each do |keytype|
+    context "keytype #{keytype}" do
+      let(:node) { 'foo.example.com' }
+      let(:params) {
+        {
+          'keytype' => keytype,
+          'private_key' => 'private key',
+          'public_key' => 'public key',
+          'public_cert' => 'public cert',
+          'mode' => '0666',
+          'owner' => 'ssh-key',
+          'group' => 5,
+          'ssh_etc' => '/opt/ssh/etc',
+        } }
+      case keytype
+      when 'rsa'
+        keyfile = 'ssh_host_rsa_key'
+        public_key = "ssh-rsa public key foo.example.com\n"
+      when 'dsa'
+        keyfile = "ssh_host_dsa_key"
+        public_key = "ssh-dss public key foo.example.com\n"
+      when 'ecdsa'
+        keyfile = "ssh_host_ecdsa_key"
+        public_key = "ecdsa-sha2-nistp256 public key foo.example.com\n"
+      when 'ed25519'
+        keyfile = "ssh_host_ed25519_key"
+        public_key = "ssh-ed25519 public key foo.example.com\n"
+      end
+      
+      it { is_expected.to contain_file("/opt/ssh/etc/#{keyfile}").with('mode' => '0666','owner' => 'ssh-key', 'group' => 5,'content' => 'private key') }
+      it { is_expected.to contain_file("/opt/ssh/etc/#{keyfile}.pub").with('mode' => '0644','owner' => 'ssh-key', 'group' => 5).with_content(public_key) }
+      it { is_expected.to contain_file("/opt/ssh/etc/#{keyfile}-cert.pub").with('mode' => '0644','owner' => 'ssh-key', 'group' => 5).with_content("public cert\n") }
+    end
+  end
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:params) do
+        { 'keytype' => 'rsa', 'private_key' => nil, 'public_key' => nil }
+      end
       let(:facts) { os_facts }
       let :pre_condition do
         'include openssh'
